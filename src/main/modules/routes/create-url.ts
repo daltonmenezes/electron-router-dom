dd7dc2731db3517beaa70356fea153cad5315db2
Dalton Menezes <daltonmenezes@outlook.com>
2022-11-02 18:35:28 -0300

    chore: handle extra double slash cases on `createURLRoute` - fixes #3

--- src/main/modules/routes/create-url.ts
+++ src/main/modules/routes/create-url.ts
@@ -1,3 +1,7 @@
+import { removeURLExtraDoubleSlashes } from 'src/shared'
+
 export function createURLRoute(route: string, id: string) {
-  return `${route}/#/${id}`
+  const URL = `${route}/#/${id}`
+
+  return removeURLExtraDoubleSlashes(URL)
 }
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,1 +1,2 @@
+export * from './utils/remove-url-extra-double-slashes'
 export * from './utils/to-lower-case-keys'
