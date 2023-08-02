580a7190d7834710841aedbec6f17ad6c2df8d0e
Dalton Menezes <daltonmenezes@outlook.com>
2023-08-02 18:41:08 -0300

    chore: update rollup config to mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,11 +2,13 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import esbuild from 'rollup-plugin-esbuild'
+import { createRequire } from 'module'
 import dts from 'rollup-plugin-dts'
 
+const require = createRequire(import.meta.url)
 const packageJson = require('./package.json')
 
-const external = ['react', 'react-router-dom', 'electron']
+const external = ['react', 'react-router-dom', 'electron', 'react-dom']
 
 /** @type {import('rollup').RollupOptions} */
 export default [
